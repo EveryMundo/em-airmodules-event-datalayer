e1456b8626a23e23deced84963b29a5398265c62
KaylingW <kwong028@fiu.edu>
2021-03-22 17:00:02 -0400

    Added more formatting. FIX numeric formatting

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const airModulesDataLayer = {
-  event: "viewable-impression",
-  module: "em-booking-popup-abstract",
-  eventAction: "viewable - impression",
+  event: "viewable impression",
+  module: "em booking popup abstract",
+  eventAction: "viewableimpression",
   actionLabel: null,
-  airlineIataCode: "",
-  provider: "SriLankanAirlines",
-  journeyType: "ROUND_TRIP",
+  airlineIataCode: "ul",
+  provider: "sri lankan airlines",
+  journeyType: "ow",
   originAirportIataCode: "CMB",
   destinationAirportIataCode: "SIN",
   route: "CMB>SIN",
   currencyCode: "LKR",
   totalPrice: null,
   totalPriceUSD: null,
-  fareClass: "ECONOMY",
+  fareClass: "ec",
   departureDate: "03/13/2021",
   returnDate: "2021-06-14",
   daysUntilFlight: 25,
@@ -22,7 +22,7 @@ const airModulesDataLayer = {
   deeplinkSiteEdition: null,
   miles: null,
   timestamp: "2021-02-16T17:41:43.200Z",
-  url: "https: //www.srilankan.com/en-lk/",
+  url: "https://www.srilankan.com/en-lk/",
   passenger: [
     {
       count: 1,
@@ -54,144 +54,215 @@ const airModulesDataLayer = {
 };
 
 const formatAll = (obj) => {
-  addParameters(obj)
- }
+  return (
+    addParameters(obj),
+    formatJourney(obj),
+    formatFareClass(obj),
+    formatProvider(obj),
+    formatCase(obj),
+    formatDate(obj),
+    formatUrl(obj)
+  );
+};
 
- const addParameters =(obj) =>{
-  if(!(obj.hasOwnProperty("moduleId"))){
+const addParameters = (obj) => {
+  if (!obj.hasOwnProperty("moduleId")) {
     obj.moduleId = "";
   }
-  if(!(obj.hasOwnProperty("tagName"))){
+  if (!obj.hasOwnProperty("tagName")) {
     obj.tagName = "";
   }
   return obj;
-}
+};
 
-const formatter = { formatAll, addParameters };
+const formatJourney = (obj) => {
+  if (obj.journeyType.match(/(oneway|one-way|one_way|ow|one way)/gi)) {
+    obj.journeyType = "ONE_WAY";
+  } else if (
+    obj.journeyType.match(/(roundtrip|round-trip|round_trip|rt|round trip)/gi)
+  ) {
+    obj.journeyType = "ROUND_TRIP";
+  } else {
+    return (obj.journeyType = "");
+  }
+  return obj;
+};
 
+const formatFareClass = (obj) => {
+  if (obj.fareClass.match(/(economy|ec|e)/gi)) {
+    obj.fareClass = "ECONOMY";
+  } else if (obj.fareClass.match(/(business|bc|b|businessclass)/gi)) {
+    obj.fareClass = "BUSINESS";
+  } else if (obj.fareClass.match(/(first|fc|f|firstclass)/gi)) {
+    obj.fareClass = "FIRST";
+  } else {
+    obj.fareClass = "";
+  }
+  return obj;
+};
 
+//can only format to capital letters if airline name is separated by spaces
+const formatProvider = (obj) => {
+  let airlineName = obj.provider;
+  let finalAirlineName = "";
 
+  if (airlineName.match(/[a-zA-Z0-9]+/g)) {
+    const airlineArray = airlineName ? airlineName.split(" ") : [];
+    if (airlineArray && airlineArray.length > 0) {
+      airlineArray.forEach((key) => {
+        finalAirlineName +=
+          key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
+      });
+      return (obj.provider = finalAirlineName);
+    } else {
+      return "";
+    }
+  }
+};
 
+const formatCase = (obj) => {
+  let keyArr = [
+    "event", //kebab case
+    "module", //kebab case
+    "eventAction", //spaced kebab case
+    "airlineIataCode",
+    "originAirportIataCode",
+    "destinationAirportIataCode",
+    "currencyCode",
+    "route",
+    "countryIsoCode",
+    "cityCode",
+    "languageIsoCode",
+    "siteEdition",
+    "name", //lodging name gets titlecased.
+  ];
 
+  keyArr.forEach((key) => {
+    if (key == "event" || key == "module") {
+      obj[key] = obj[key]
+        .replace(/([a-z])([A-Z])/g, "$1-$2")
+        .replace(/[\s_]+/g, "-")
+        .toLowerCase();
+    } else if (key == "eventAction") {
+      obj.eventAction = obj.event.split("-").join(" - ");
+    } else if (
+      key == "countryIsoCode" ||
+      key == "cityCode" ||
+      key == "languageIsoCode" ||
+      key == "siteEdition"
+    ) {
+      //Lodging casing
+      obj.lodging[0].cityCode = obj.lodging[0].cityCode.toUpperCase();
 
+      //Page casing
+      obj.page[0].countryIsoCode = obj.page[0].countryIsoCode.toUpperCase();
+      obj.page[0].languageIsoCode = obj.page[0].languageIsoCode.toLowerCase();
+      obj.page[0].siteEdition =
+        obj.page[0].languageIsoCode.toLowerCase() +
+        "-" +
+        obj.page[0].countryIsoCode.toUpperCase();
+    } else if (key == "name") {
+      obj.lodging[0].name =
+        obj.lodging[0].name.charAt(0).toUpperCase() +
+        obj.lodging[0].name.substr(1).toLowerCase();
+    } else {
+      obj[key] = obj[key].toUpperCase();
+    }
+  });
+  return obj;
+};
 
-  // formatJourney(obj) {
-  //   var journey = obj.journeyType;
-  //   if (journey.match(/(oneway|one-way|one_way|ow|one way)/gi)) {
-  //     return "ONE_WAY";
-  //   } else if (
-  //     journey.match(/(roundtrip|round-trip|round_trip|rt|round trip)/gi)
-  //   ) {
-  //     return "ROUND_TRIP";
-  //   } else {
-  //     return "";
-  //   }
-  //   return journey;
-  // },
+const formatDate = (obj) => {
+  let dateArr = [
+    "departureDate",
+    "returnDate",
+    "timestamp",
+    "startDate",
+    "endDate",
+  ];
+  dateArr.forEach((key) => {
+    if (key == "timestamp") {
+      obj[key] = new Date(obj[key]).toISOString();
+    } else if (key == "startDate" || key == "endDate") {
+      obj.lodging[0][key] = new Date(obj.lodging[0][key])
+        .toISOString()
+        .substr(0, 10);
+    } else {
+      obj[key] = new Date(obj[key]).toISOString().substr(0, 10);
+    }
+  });
+  return obj;
+};
 
-  // formatFareClass(obj) {
-  //   var fareClass = obj.fareClass;
-  //   if (fareClass.match(/(economy|ec|e)/gi)) {
-  //     return "ECONOMY";
-  //   } else if (fareClass.match(/(business|bc|b|businessclass)/gi)) {
-  //     return "BUSINESS";
-  //   } else if (fareClass.match(/(first|fc|f|firstclass)/gi)) {
-  //     return "FIRST";
-  //   } else {
-  //     return "";
-  //   }
-  //   return fareClass;
-  // },
+const formatUrl = (obj) => {
+  let newUrl = obj.url.split(":").join(": ");
+  obj.url = newUrl;
 
-  // //can only format to capital letters if airline name is separated by spaces
-  // formatProvider(obj) {
-  //   let airlineName = obj.provider;
-  //   let finalAirlineName = "";
-  //   airlineName = "sri lankan airlines";
+  return obj;
+};
 
-  //   if (airlineName.match(/[a-zA-Z0-9]+/g)) {
-  //     const airlineArray = airlineName ? airlineName.split(" ") : [];
-  //     if (airlineArray && airlineArray.length > 0) {
-  //       airlineArray.forEach((key) => {
-  //         finalAirlineName +=
-  //           key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
-  //       });
-  //       return finalAirlineName;
-  //     } else {
-  //       return "";
-  //     }
-  //   }
-  // },
+// const formatNumbers = (obj) => {
+//   let numArr = [
+//     "totalPrice",
+//     "totalPriceUSD",
+//     "daysUntilFlight",
+//     "miles",
+//     "count",
+//     "adultCount",
+//     "youngAdultCount",
+//     "childCount",
+//     "infantInLapCount",
+//     "infantInSeatCount",
+//     "roomCount",
+//     "starRating",
+//   ];
 
+//   let error = "Value is not a number.";
 
-  // formatCase(obj) {
-  //   let keyArr = [
-  //     "airlineIataCode",
-  //     "originAirportIataCode",
-  //     "destinationAirportIataCode",
-  //     "currencyCode",
-  //     "route",
-  //     "countryIsoCode",
-  //     "cityCode",
-  //     "languageIsoCode",
-  //     "siteEdition",
-  //     "name", //lodging name gets titlecased.
-  //   ];
-
-  //   keyArr.forEach((key) => {
-  //     if (key == "countryIsoCode" || key == "cityCode" || key== "languageIsoCode" || key== "siteEdition") {
-  //       //Lodging casing
-  //       obj.lodging[0].cityCode = obj.lodging[0].cityCode.toUpperCase();
-
-  //       //Page casing
-  //       obj.page[0].countryIsoCode = obj.page[0].countryIsoCode.toUpperCase();
-  //       obj.page[0].languageIsoCode = obj.page[0].languageIsoCode.toLowerCase();
-  //       obj.page[0].siteEdition = obj.page[0].languageIsoCode.toLowerCase() + "-" + obj.page[0].countryIsoCode.toUpperCase();
-
-  //     } else if (key == "name") {
-  //       obj.lodging[0].name =
-  //         obj.lodging[0].name.charAt(0).toUpperCase() +
-  //         obj.lodging[0].name.substr(1).toLowerCase();
-  //      } else {
-  //       obj[key] = obj[key].toUpperCase();
-  //     }
-  //   });
-  //   return obj;
-  // },
-
-  // formatDate(obj) {
-  //   let dateArr = [
-  //     "departureDate",
-  //     "returnDate",
-  //     "timestamp",
-  //     "startDate",
-  //     "endDate",
-  //   ];
-  //   dateArr.forEach((key) => {
-  //     if (key == "timestamp") {
-  //       obj[key] = new Date(obj[key]).toISOString();
-  //     } else if (key == "startDate" || key == "endDate") {
-  //       obj.lodging[0][key] = new Date(obj.lodging[0][key])
-  //         .toISOString()
-  //         .substr(0, 10);
-  //     } else {
-  //       obj[key] = new Date(obj[key]).toISOString().substr(0, 10);
-  //     }
-  //   });
-  //   return obj;
-  // },
-
-  // formatUrl(obj) {
-  //   var newUrl = obj.url.split(":").join(": ");
-
-  //   return newUrl;
-  // },
-
- 
+//   numArr.forEach((key) => {
+//     if (
+//       typeof obj[key] == "string" ||
+//       typeof obj.passenger[0][key] == "string" ||
+//       typeof obj.lodging[0][key] == "string"
+//     ) {
+//       const parsedKey = parseInt(obj[key]);
+//       const parsedPassenger = parseInt(obj.passenger[0][key]);
+//       const parsedLodging = parseInt(obj.lodging[0][key]);
+//       if (
+//         !isNaN(parsedKey) ||
+//         !isNaN(parsedPassenger) ||
+//         !isNaN(parsedLodging)
+//       ) {
+//         return parsedKey, parsedPassenger, parsedLodging;
+//       } else {
+//         throw error;
+//       }
+//     }
+//     if (
+//       !obj[key]||
+//       !obj.passenger[0][key]||
+//       !obj.lodging[0][key]
+//     ) {
+//       return "";
+// 			console.log(obj[key]);
+//     }
+//     return obj;
+//   });
 // };
 
-console.log(formatter.formatAll(airModulesDataLayer));
-// console.log(formatter.addParameters(airModulesDataLayer));
+const formatter = {
+  formatAll,
+  addParameters,
+  formatJourney,
+  formatFareClass,
+  formatProvider,
+  formatCase,
+  formatDate,
+  formatUrl,
+  // formatNumbers,
+};
+console.log("OUTPUT", formatter.formatAll(airModulesDataLayer));
 
+// console.log(formatNumbers(airModulesDataLayer));
 
 export default formatter;
