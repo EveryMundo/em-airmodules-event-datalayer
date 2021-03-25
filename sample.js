cda73e473717ba0f9c639ae2352b2503de24c68c
KaylingW <kwong028@fiu.edu>
2021-03-25 17:51:21 -0400

    Complete

---
+++ sample.js
@@ -0,0 +1,56 @@
+const formatter = require("./index.js");
+const airModulesDataLayer = {
+  event: "viewable impression",
+  module: "emBookingPopupAbstract",
+  eventAction: "viewable - impression",
+  actionLabel: null,
+  airlineIataCode: "ul",
+  provider: "sri lankan airlines",
+  journeyType: "ow",
+  originAirportIataCode: "CMB",
+  destinationAirportIataCode: "SIN",
+  route: "cmb>sin",
+  currencyCode: "LKR",
+  totalPrice: "5.21",
+  totalPriceUSD: null,
+  fareClass: "ec",
+  departureDate: "03/13/2021",
+  returnDate: "2021-06-14",
+  daysUntilFlight: "25", //25
+  tripLength: 93,
+  isFlexibleDates: null,
+  discountCode: null,
+  deeplinkSiteEdition: null,
+  miles: null,
+  timestamp: "2021-02-16",
+  url: "https://www.srilankan.com/en-lk/",
+  passenger: [
+    {
+      count: 1,
+      adultCount: "1",
+      youngAdultCount: null,
+      childCount: null,
+      infantInLapCount: null,
+      infantInSeatCount: null,
+    },
+  ],
+  page: [
+    {
+      siteEdition: "en-LK",
+      countryIsoCode: "lk",
+      languageIsoCode: "en",
+    },
+  ],
+  lodging: [
+    {
+      cityCode: "sin",
+      name: "intercontinental",
+      startDate: "2021/03/13",
+      endDate: "2021-03-20",
+      roomCount: 2,
+      tripLength: 7,
+      starRating: 5,
+    },
+  ],
+};
+formatter.formatAll(airModulesDataLayer);
